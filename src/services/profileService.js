b7bf4763fa70b12f53e1bc3104f8ca4411c3ec65
cmosley <cmosley@beholdmedia.com>
2021-11-03 17:26:16 -0500

    CRUD and retrival functions for profileService API

---
+++ src/services/profileService.js
@@ -0,0 +1,42 @@
+import tokenService from '../services/tokenService';
+const BASE_URL = '/api/profiles/';
+
+export function create(profile) {
+  return fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+          'content-type': 'application/json', 
+          'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(profile)
+  }, {mode: "cors"})
+  .then(res => res.json());
+}
+
+// check to see if user has profile, used to render profile page empty or full
+export function getAllByCurrentUser(userid) {
+  return fetch(`${BASE_URL}user/${userid}`, {mode: 'cors'})
+      .then(res => res.json())
+}
+
+export function getCurrentUserProfile(id) {
+  return fetch(`${BASE_URL}${id}`, {mode: 'cors'})
+      .then(res => res.json())
+}
+
+export function deleteProfile(id) {
+  return fetch(`${BASE_URL}${id}`, {
+      method: 'DELETE',
+      headers: {'Authorization': `Bearer ${tokenService.getToken()}`},
+  }, {mode: 'cors'})
+  .then(res => res.json())
+}
+
+export function update(profile) {
+  return fetch(`${BASE_URL}${profile._id}`, {
+      method: "PUT", 
+      headers: {'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken()},
+      body: JSON.stringify(profile)
+  }, {mode: 'cors'})
+  .then(res => res.json());
+}
--- src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import tokenService from "../services/tokenService"
-const BASE_URL = "/api/users/"
-
-export function getAllUsers() {
-  return fetch(BASE_URL, {
-    headers: { Authorization: "Bearer " + tokenService.getToken()}
-  }, { mode: "cors" })
-  .then(res => res.json())
-}
