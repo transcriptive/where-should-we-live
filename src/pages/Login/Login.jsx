074365f8ebba403863d5cb37037f32b7d5a82ae8
pdbrooks <PDBrooks01@outlook.com>
2021-11-03 21:58:53 -0400

    Updated Login page styling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -32,7 +32,7 @@ export default function Login (props) {
   };
 
     return (
-      <div className="min-h-screen bg-white flex">
+      <div className="min-h-screen bg-white flex max-w-7xl font-montserrat-bold mx-auto">
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div className="flex justify-center">
@@ -100,7 +100,7 @@ export default function Login (props) {
           </div>
         </div>
       </div>
-      <div className="hidden lg:block relative w-0 flex-1">
+      <div className="hidden lg:block relative flex-1">
         <img
           className="absolute inset-0 h-full w-full object-cover"
           src="/images/rezaul-karim-102abqkKhbY-unsplash.jpg"
@@ -113,3 +113,5 @@ export default function Login (props) {
 
 
 
+  
+ 
