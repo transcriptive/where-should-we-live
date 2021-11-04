0beeb59096f01c3a3589793f1868b0f8bacb4d2a
pdbrooks <PDBrooks01@outlook.com>
2021-11-03 22:12:26 -0400

    Updated  tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,35 @@ module.exports = {
       'sliderBlue':'#DAE4EC',
       'asphalt':'#728A9F',
     }),
+    inset:{
+      '561':'35rem',
+      '188':'11.75rem',
+      '70':'4.375rem',
+      '227':'14.1875rem',
+      '94':'5.875rem',
+      '34':'2.125rem',
+      '266':'16.625rem',
+      '321':'20.0625rem',
+      '196':'12.25rem',
+      '365':'22.8125rem',
+      '146':'9.125rem',
+      '31':'1.9375rem',
+      '219':'13.6875rem',
+      '1095':'68.4375rem',
+
+    },
+    fontSize:{
+      '24':['24px','36px'],
+      '28':'1.75rem',
+      '36':['36px','44px'],
+      '52':['52px','64px'],
+
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         montserrat: ['Montserrat'],
+        roboto: ['Roboto'],
       },
     },
   },
