d58ff53e9a4b6badf9485bc6a325059b20ba6d5c
John Raymond <jray014@gmail.com>
2021-11-04 15:24:03 -0400

    added return to map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
-import { GoogleMap } from "react-google-maps";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
-function Map() {
+function setMap() {
     return (
         <GoogleMap 
             defaultZoom={10}
             defaultCenter={{ lat: 37.0902, lng: 95.7129}}
         />
+    )
+}
+
+const fullMap = withScriptjs(withGoogleMap(setMap));
+
+export default function Map(props) {
+    return (
+        <>
+            <fullMap
+                googleMapURL={""}
+            />
+        </>
     )
 }
