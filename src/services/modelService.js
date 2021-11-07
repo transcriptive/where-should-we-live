2c73a9fde52d4795d554c10b97192efc145eab69
cmosley <cmosley@beholdmedia.com>
2021-11-06 19:06:49 -0500

    update fetch for model

--- src/services/modelService.js
+++ src/services/modelService.js
@@ -1,6 +1,5 @@
-const BASE_URL = '/model'
+const BASE_URL = 'https://product-club-us-census.herokuapp.com//model'
 
-// https://product-club-us-census.herokuapp.com/
 
 // fetchData() posts a request to the data model with user input values on SearchForm. Flask backend, locally at port 5000, processes values through county(), sends the results back as the response, converts to json, returns result to handleSubmit()
 export function fetchData(state) {
