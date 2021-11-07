3d72dde0a12ed70a2b77d2a313e3c76a7d8b26d8
cmosley <cmosley@beholdmedia.com>
2021-11-07 10:32:56 -0600

    Restructure wikiService fetch, receiving data

--- src/services/modelService.js
+++ src/services/modelService.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'https://product-club-us-census.herokuapp.com//model'
-
+const BASE_URL = 'https://product-club-us-census.herokuapp.com/model'
+// 
 
 // fetchData() posts a request to the data model with user input values on SearchForm. Flask backend, locally at port 5000, processes values through county(), sends the results back as the response, converts to json, returns result to handleSubmit()
 export function fetchData(state) {
@@ -9,9 +9,7 @@ export function fetchData(state) {
     cache: "no-cache", 
     headers:{
       "Content-Type":"application/json", 
-      "Access-Control-Allow-Credentials": true,
     }, 
-    mode: "no-cors",
     body:JSON.stringify({state})
   })
   .then(response => response.json())
