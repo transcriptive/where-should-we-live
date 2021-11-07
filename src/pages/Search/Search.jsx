78c08186906d57b34a26e031cdd3c66166cd13b6
cmosley <cmosley@beholdmedia.com>
2021-11-07 15:55:56 -0600

    Preloader install

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from '../../hooks/useForm'
 import { fetchData } from "../../services/modelService"
 import SearchForm from "../../components/SearchForm/SearchForm";
 import ResultsCarousel from "../../components/Carousel/Carousel";
 import FormResults from "../../components/FormResults/FormResults";
+import PreLoader from "../../components/PreLoader/PreLoader";
+import MapPinLocation from '../../media/map-pin-location.json';
+import Success from '../../media/success-check.json';
 
 export default function Search() {
   const [modelData, SetModelData] = useState([]);
+  const [loading, setLoading] = useState(undefined);
+  const [completed, setCompleted] = useState(undefined);
+
   const [state, handleChange] = useForm({
     income: 250000,
     climate: 75,  
@@ -16,21 +22,34 @@ export default function Search() {
 
 
   // fetchData submits slider values to data model on flask backend. Flask returns response with all county information, values stored in modelData -cm
-  const handleSubmit = async (e) => {
+  useEffect(() =>  {
+    async function updateResults() {
+      const json = await fetchData(state) 
+      console.log(json, 'json returned')
+      const arr = Object.entries(json.model_results)
+      const results = []
+        for (const i in Object.entries(arr[0][1])) {
+          const entry = arr.reduce((acc, item) => {
+            acc[item[0]] = item[1][i]
+            return acc
+          }, {})
+          results.push(entry)
+        }
+      SetModelData(results)
+    }
+      updateResults().catch(error => console.log(error))
+  }, [state])
+
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const json = await fetchData(state)
-    console.log(json, 'json returned')
-    const arr = Object.entries(json.model_results)
-    const results = []
-    for (const i in Object.entries(arr[0][1])) {
-      const entry = arr.reduce((acc, item) => {
-        acc[item[0]] = item[1][i]
-        return acc
-      }, {})
-    results.push(entry)
-  }
-    SetModelData(results)
-  }
+    SetModelData(modelData)
+    console.log(state, 'state')
+    console.log(modelData, 'modeldata')
+  };
+
+     
+ 
 
 
   return (
@@ -42,23 +61,28 @@ export default function Search() {
         handleSubmit={handleSubmit} 
         handleChange={handleChange}
       />
-      
-      { modelData.length === true 
-        ? 
-        (<>
+      {!completed ? (
+        <>
+          {!loading ? (
+            <PreLoader data={MapPinLocation} />
+          ) : (
+            <PreLoader data={Success} />
+          )}
+        </>
+      ) : (
+        <>
           <ResultsCarousel 
             results={modelData}
           />
           <FormResults 
-          results={modelData}
+             results={modelData}
           />
         </>
-        )
-        :
-        <div></div>
-        
-      }
-      
+      )}
+
+     
+   
+     
 
     </>
   )
