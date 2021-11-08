76ad3ca79ea5d01feca2bf5a03e6ab4416c8b631
pdbrooks <PDBrooks01@outlook.com>
2021-11-07 22:41:11 -0500

    Modal is showing and ready for backend integration

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
+import { Dialog, Transition } from '@headlessui/react';
 import { useForm } from "../../hooks/useForm";
 import * as profileService from "../../services/profileService";
 
@@ -37,15 +38,17 @@ export default function Profile(props) {
   }, []);
 
   // hook to check form validity
-  useEffect(() => {
-    formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
-  }, [state]);
+  // useEffect(() => {
+  //   formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
+  // }, [state]);
 
   // pass form data via submit to handleAddprofile func
   async function handleSubmit(e) {
     e.preventDefault();
     handleAddProfile(state);
   }
+  // For Modal
+  const [showModal, setShowModal] = React.useState(false)
 
   const dummyData = [
     "Tarrant County, TX",
@@ -56,15 +59,16 @@ export default function Profile(props) {
 
   return (
     <main className="mx-auto flex justify-center items-center">
-      {/* Modal for Content change */}
-      <div class="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id">
+      {/* Modal for Profile ontent change */}
+      {showModal ? (
+      <div class="min-w-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id">
         <div class="absolute bg-black opacity-80 inset-0 z-0"></div>
         <div class="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
           {/* <!--content--> */}
           <div class="">
             {/* <!--body--> */}
-            <div class="text-center p-5 flex-auto justify-center">
-              <h2 class="text-xl font-bold py-4 ">Profile Settings</h2>
+            <div class="text-center flex-auto justify-center">
+              <h2 class="text-xl font-bold p-2 ">Profile Settings</h2>
               <div className="mt-1 sm:mt-0 sm:col-span-2">
                 Edit your account details below 
               </div>
@@ -78,14 +82,14 @@ export default function Profile(props) {
               {/* For simplicity, each section is seperated by its own empty div */}
               <div className="">
                   {/* TOP SECTION  */}
-                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5 mb-2">
                       <label
                       htmlFor="name"
-                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      className="block text-sm font-medium text-gray-700 "
                       >
                       Name:
                       </label>
-                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <div className="mt-1 sm:mt-0 sm:col-span-2 self-center">
                       <input
                           type="text"
                           name="name"
@@ -96,14 +100,14 @@ export default function Profile(props) {
                       />
                       </div>
                   </div>
-                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start mb-2 ">
                       <label
                       htmlFor="email"
-                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      className="block text-sm font-medium text-gray-700"
                       >
                       Email
                       </label>
-                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <div className="mt-1 sm:mt-0 sm:col-span-2 self-center">
                       <input
                           type="text"
                           name="email"
@@ -115,14 +119,14 @@ export default function Profile(props) {
                       </div>
                   </div>
 
-                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start mb-2">
                       <label
                       htmlFor="movingFrom"
-                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      className="block text-sm font-medium text-gray-700"
                       >
                       Moving From:
                       </label>
-                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <div className="mt-1 sm:mt-0 sm:col-span-2 self-center">
                       <input
                           type="text"
                           name="movingFrom"
@@ -134,21 +138,21 @@ export default function Profile(props) {
                       
                       </div>
                   </div>
-                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start mb-2">
                       <label
                       htmlFor="photo"
-                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      className="block text-sm font-medium text-gray-700"
                       >
                       Profile Photo:
                       </label>
-                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <div className="mt-1 sm:mt-0 sm:col-span-2 self-center">
                       <input
                           type="file"
                           name="photo"
                           id="photo"
                           value={state.photo}
                           onChange={handleChange}
-                          className="px-2 max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                          className="max-w-lg block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-none"
                       />
                       </div>
                   </div>
@@ -159,7 +163,7 @@ export default function Profile(props) {
                   </div>
                   {/* PREFERENCES SECTION  */}
                   <div>
-                  <div className="sm:grid sm:grid-cols-1 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5 h-auto">
+                  <div className="sm:grid sm:grid-cols-1 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-2 h-auto">
                   <div>
                       <h3 className="mt-2 text-lg leading-6 font-medium text-gray-900">
                       Preferences
@@ -170,7 +174,7 @@ export default function Profile(props) {
                   <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
                     <label
                         htmlFor="language"
-                        className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                        className="block text-sm font-medium text-gray-700"
                     >
                         Language
                     </label>
@@ -191,7 +195,7 @@ export default function Profile(props) {
                   </div>
                   <label
                       htmlFor="country"
-                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      className="block text-sm font-medium text-gray-700 "
                   >
                       Date Format
                   </label>
@@ -201,7 +205,7 @@ export default function Profile(props) {
                       name="dateFormat"
                       value={state.dateFormat}
                       onChange={handleChange}
-                      className="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                      className="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm "
                       >
                       <option value='DD-MM-YYYY'>DD-MM-YYYY</option>
                       <option value='MM-DD-YYYY'>MM-DD-YYYY</option>
@@ -217,16 +221,17 @@ export default function Profile(props) {
             </div>
             {/* <!--footer--> */}
             <div class="p-3  mt-2 text-center space-x-4 md:block">
-                <button class="mb-2 md:mb-0 bg-red-400 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-white rounded-full hover:shadow-lg hover:bg-red-900">
+                <button class="mb-2 md:mb-0 bg-red-600 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-white rounded-full hover:shadow-lg hover:bg-red-900" type="button"
+                    onClick={() => setShowModal(false)}>
                     Cancel
                 </button>
-                <button class="mb-2 md:mb-0 bg-primary border border-primary px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-yolk hover:text-black">Save</button>
+                <button class="mb-2 md:mb-0 bg-primary border border-primary px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-yolk hover:text-black" type="button"
+                    onClick={() => setShowModal(false)}>Save</button>
             </div>
           </div>
         </div>
       </div>
-
-
+      ) : null}
 
 
 
@@ -265,11 +270,11 @@ export default function Profile(props) {
         <div class="w-1/5 h-auto mb-0 float-right">
           <div class="flex-1 h-full">
             <div class="flex items-center justify-center flex-1 h-full p-2 text-white shadow rounded-lg bg-primary hover:bg-yolk focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yolk">
-              <div class="relative">
+              <button class="relative" type="button" onClick={() => setShowModal(true)}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-black-800" viewBox="0 0 20 20" fill="currentColor">
                   <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                 </svg>
-              </div>
+              </button>
             </div>
           </div>
         </div>
