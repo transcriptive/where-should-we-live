b929a4015dc4bf225a803b4d298478ec9213796c
cmosley <cmosley@beholdmedia.com>
2021-11-08 00:25:51 -0600

    debug deployment

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -8,7 +8,7 @@ import PreLoader from "../../components/PreLoader/PreLoader";
 import MapPinLocation from '../../media/map-pin-location.json';
 import Success from '../../media/success-check.json';
 import { Wrapper } from "@googlemaps/react-wrapper";
-
+const GOOGLE_API = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 export default function Search({user}) {
   const [modelData, setModelData] = useState([]);
@@ -56,7 +56,7 @@ export default function Search({user}) {
         handleChange={handleChange}
       />
       <Wrapper 
-          apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} 
+          apiKey={GOOGLE_API} 
           version="weekly"
           libraries={["places"]}
         >
