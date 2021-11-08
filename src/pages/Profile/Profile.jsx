4c56797fd1fa7b687302d4277f0595b7bee8305e
pdbrooks <PDBrooks01@outlook.com>
2021-11-07 20:21:21 -0500

    Added modal to profile. Still updating connection

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -28,18 +28,18 @@ export default function Profile(props) {
   }
   
   // function to determine if user has profile already, if yes, redirect to edit page
-  // useEffect(() => {
-  //   const hasProfile = async () => {
-  //     const hasData = await profileService.getAllByCurrentUser(props.user._id)
-  //     if (hasData?.name)history.push('/account/edit')
-  //   } 
-  //   hasProfile()
-  // }, []);
+  useEffect(() => {
+    const hasProfile = async () => {
+      const hasData = await profileService.getAllByCurrentUser(props.userid)
+      if (hasData?.name)history.push('/account/edit')
+    } 
+    hasProfile()
+  }, []);
 
   // hook to check form validity
-  // useEffect(() => {
-  //   formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
-  // }, [state]);
+  useEffect(() => {
+    formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
+  }, [state]);
 
   // pass form data via submit to handleAddprofile func
   async function handleSubmit(e) {
@@ -56,6 +56,180 @@ export default function Profile(props) {
 
   return (
     <main className="mx-auto flex justify-center items-center">
+      {/* Modal for Content change */}
+      <div class="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id">
+        <div class="absolute bg-black opacity-80 inset-0 z-0"></div>
+        <div class="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
+          {/* <!--content--> */}
+          <div class="">
+            {/* <!--body--> */}
+            <div class="text-center p-5 flex-auto justify-center">
+              <h2 class="text-xl font-bold py-4 ">Profile Settings</h2>
+              <div className="mt-1 sm:mt-0 sm:col-span-2">
+                Edit your account details below 
+              </div>
+              {/* FORM START */}
+              <form ref={formRef} onSubmit={handleSubmit}
+              className=""
+              >
+              {/* Styling for FORM divs begins */}
+
+              <div className="">
+              {/* For simplicity, each section is seperated by its own empty div */}
+              <div className="">
+                  {/* TOP SECTION  */}
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                      <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      >
+                      Name:
+                      </label>
+                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <input
+                          type="text"
+                          name="name"
+                          id="name"
+                          value={state.name}
+                          onChange={handleChange}
+                          className="px-2 max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                      />
+                      </div>
+                  </div>
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                      <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      >
+                      Email
+                      </label>
+                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <input
+                          type="text"
+                          name="email"
+                          id="email"
+                          value={state.email}
+                          onChange={handleChange}
+                          className="px-2 max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                      />
+                      </div>
+                  </div>
+
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                      <label
+                      htmlFor="movingFrom"
+                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      >
+                      Moving From:
+                      </label>
+                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <input
+                          type="text"
+                          name="movingFrom"
+                          id="movingFrom"
+                          value={state?.movingFrom}
+                          onChange={handleChange}
+                          className="px-2 max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                      />
+                      
+                      </div>
+                  </div>
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                      <label
+                      htmlFor="photo"
+                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                      >
+                      Profile Photo:
+                      </label>
+                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <input
+                          type="file"
+                          name="photo"
+                          id="photo"
+                          value={state.photo}
+                          onChange={handleChange}
+                          className="px-2 max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                      />
+                      </div>
+                  </div>
+                  {/* GROUP SECTION  */}
+                  {/* need to create list of groups to join with checkbox save feature */}
+                  <div>
+                  
+                  </div>
+                  {/* PREFERENCES SECTION  */}
+                  <div>
+                  <div className="sm:grid sm:grid-cols-1 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5 h-auto">
+                  <div>
+                      <h3 className="mt-2 text-lg leading-6 font-medium text-gray-900">
+                      Preferences
+                      </h3>
+                  </div>
+                  </div>
+
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+                    <label
+                        htmlFor="language"
+                        className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                    >
+                        Language
+                    </label>
+                  <div className="mt-1 sm:mt-0 sm:col-span-2">
+                    <select
+                          type="text"
+                          name="language"
+                          id="language"
+                          value={state?.language}
+                          onChange={handleChange}>
+                      <option value="english"> English </option>
+                      <option value="arabic"> Arabic </option>
+                      <option value="chinese"> Chinese </option>
+                      <option value="japanese"> Japanese</option>
+                      <option value="portugese"> Portuguese </option>
+                      <option value="spanish"> Spanish </option>
+                    </select>
+                  </div>
+                  <label
+                      htmlFor="country"
+                      className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                  >
+                      Date Format
+                  </label>
+                  <div className="mt-1 sm:mt-0 sm:col-span-2">
+                      <select
+                      id="dateFormat"
+                      name="dateFormat"
+                      value={state.dateFormat}
+                      onChange={handleChange}
+                      className="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                      >
+                      <option value='DD-MM-YYYY'>DD-MM-YYYY</option>
+                      <option value='MM-DD-YYYY'>MM-DD-YYYY</option>
+                      </select>
+                  </div>
+                  </div>   
+                  </div>
+                </div>
+              </div>
+              {/* Styling for FORM divs ends */}
+            </form>
+              {/* FORM END */}
+            </div>
+            {/* <!--footer--> */}
+            <div class="p-3  mt-2 text-center space-x-4 md:block">
+                <button class="mb-2 md:mb-0 bg-red-400 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-white rounded-full hover:shadow-lg hover:bg-red-900">
+                    Cancel
+                </button>
+                <button class="mb-2 md:mb-0 bg-primary border border-primary px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-yolk hover:text-black">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
       {/* Left User Info Section */}
       <div className="grid grid-flow-col gap-4 ">
       <div className="bg-primary bg-opacity-30 rounded-lg p-5 mb-2 mt-3 row-span-3">
