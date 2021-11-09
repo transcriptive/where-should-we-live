940d3464e4b6746da050bf37498f64767573471d
cmosley <cmosley@beholdmedia.com>
2021-11-09 15:26:51 -0600

    carousel styling update, google api update

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,17 +3,17 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import "./Carousel.css";
 import MapOne from './stockMapOne.jpg';
+import Texas from './states/TX.png'
+
+// function importAll(img) {
+//   let images = {};
+//   img.keys().map(item => { images[item.replace('./', '')] = img(item); });
+//   console.log(images)
+//   return images;
+// }
 
 
-function importAll(img) {
-  let images = {};
-  img.keys().map(item => { images[item.replace('./', '')] = img(item); });
-  console.log(images)
-  return images;
-}
-
-
-const images = importAll(require.context('./media/states', false, '/\.png/'));
+// const images = importAll(require.context('./media/states', false, '/\.png/'));
 
 {/* <img src={images['0.png']} /> */}
 
@@ -40,70 +40,7 @@ const responsive = {
 
 
   
-  // function convertState(county) {
-  //   // state to abbreviation
-  //   const states = [
-  //     ['Alabama', 'AL'],
-  //     ['Alaska', 'AK'],
-  //     ['American Samoa', 'AS'],
-  //     ['Arizona', 'AZ'],
-  //     ['Arkansas', 'AR'],
-  //     ['Armed Forces Americas', 'AA'],
-  //     ['Armed Forces Europe', 'AE'],
-  //     ['Armed Forces Pacific', 'AP'],
-  //     ['California', 'CA'],
-  //     ['Colorado', 'CO'],
-  //     ['Connecticut', 'CT'],
-  //     ['Delaware', 'DE'],
-  //     ['District Of Columbia', 'DC'],
-  //     ['Florida', 'FL'],
-  //     ['Georgia', 'GA'],
-  //     ['Guam', 'GU'],
-  //     ['Hawaii', 'HI'],
-  //     ['Idaho', 'ID'],
-  //     ['Illinois', 'IL'],
-  //     ['Indiana', 'IN'],
-  //     ['Iowa', 'IA'],
-  //     ['Kansas', 'KS'],
-  //     ['Kentucky', 'KY'],
-  //     ['Louisiana', 'LA'],
-  //     ['Maine', 'ME'],
-  //     ['Marshall Islands', 'MH'],
-  //     ['Maryland', 'MD'],
-  //     ['Massachusetts', 'MA'],
-  //     ['Michigan', 'MI'],
-  //     ['Minnesota', 'MN'],
-  //     ['Mississippi', 'MS'],
-  //     ['Missouri', 'MO'],
-  //     ['Montana', 'MT'],
-  //     ['Nebraska', 'NE'],
-  //     ['Nevada', 'NV'],
-  //     ['New Hampshire', 'NH'],
-  //     ['New Jersey', 'NJ'],
-  //     ['New Mexico', 'NM'],
-  //     ['New York', 'NY'],
-  //     ['North Carolina', 'NC'],
-  //     ['North Dakota', 'ND'],
-  //     ['Northern Mariana Islands', 'NP'],
-  //     ['Ohio', 'OH'],
-  //     ['Oklahoma', 'OK'],
-  //     ['Oregon', 'OR'],
-  //     ['Pennsylvania', 'PA'],
-  //     ['Puerto Rico', 'PR'],
-  //     ['Rhode Island', 'RI'],
-  //     ['South Carolina', 'SC'],
-  //     ['South Dakota', 'SD'],
-  //     ['Tennessee', 'TN'],
-  //     ['Texas', 'TX'],
-  //     ['US Virgin Islands', 'VI'],
-  //     ['Utah', 'UT'],
-  //     ['Vermont', 'VT'],
-  //     ['Virginia', 'VA'],
-  //     ['Washington', 'WA'],
-  //     ['West Virginia', 'WV'],
-  //     ['Wisconsin', 'WI'],
-  //     ['Wyoming', 'WY'],
-  // ];
+  
   // console.log(county, 'before convert')
   // county = county.replace(/\w\S*/g, function (txt) {
   //   return txt.charAt(0-1).toUpperCase() });
@@ -237,7 +174,7 @@ const dummy = [
         <Carousel
           centerMode={true}
           arrows={true}	
-          swipeable={false}
+          swipeable={true}
           draggable={false}
           showDots={false}
           responsive={responsive}
@@ -246,31 +183,32 @@ const dummy = [
           autoPlay={false}
           autoPlaySpeed={0}
           keyBoardControl={true}
-          customTransition="all .5"
-          transitionDuration={500}
+          customTransition="transform 300ms ease-in-out"
+          transitionDuration={300}
           containerClass="carousel-container"
           removeArrowOnDeviceType={["tablet", "mobile"]}
           // deviceType={this.props.deviceType}
           dotListClass="custom-dot-list-style"
           itemClass="carousel-item-padding-40-px caro-hover"
           renderButtonGroupOutside={false}
+          sliderClass='carousel-track'
           // onClick={() => handleScroll(selectedRef)}
         >
     
         {results.map((result, index) => {
           return (
             <div 
-              className={"px-4"} 
+              className={""} 
               key={index} 
               onClick={() => setSelected(index)}>
                 <img 
-                  src={MapOne
+                  src={Texas
                   }
                   className={'single-result'}
                   alt={index} 
                   key={index} 
                 ></img>
-              <p className={'ml-4 flex justify-start'}>{result.county}</p>
+              <p className={'mt-4'}>{result.county}</p>
             </div>
           )
         })}
